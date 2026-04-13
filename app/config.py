e91d97342ed2ea1be1b76fb8e6e12d19524204d8
MATRune <malik.thomas.200531@gmail.com>
2026-04-12 23:02:28 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -17,5 +17,11 @@ class Settings(BaseSettings):
     db_additional_overflow:int=10
     db_pool_timeout:int=10
     db_pool_recycle:int=10
+    ai_api_key: str = ""
+    ai_base_url: str = "https://ai-gen.sundaebytestt.com"
+    ai_model_name: str = "meta/llama-3.2-3b-instruct"
+    ai_temperature: float = 0.5
+    ai_max_tokens: int = 700
+    ai_request_timeout_seconds: int = 45
     
     model_config = SettingsConfigDict(env_file=".env")
