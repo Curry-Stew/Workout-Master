bb1591c521b7b1014bdf6c625d1e2063091297d4
K3V-TT <kjaydon11@gmail.com>
2026-04-13 02:14:51 -0400

    Update workouts.py

--- app/routers/workouts.py
+++ app/routers/workouts.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from sqlmodel import select
@@ -8,9 +10,33 @@ from . import router, templates
 
 
 @router.get("/workouts", response_class=HTMLResponse)
-async def workouts_view(request: Request, user: AuthDep, db: SessionDep):
-    workouts = db.exec(select(Workout)).all()
+async def workouts_view(
+    request: Request,
+    user: AuthDep,
+    db: SessionDep,
+    workout_type: Optional[str] = None,
+    body_part: Optional[str] = None,
+    equipment: Optional[str] = None,
+    level: Optional[str] = None,
+):
+    query = select(Workout)
+    if workout_type:
+        query = query.where(Workout.type == workout_type)
+    if body_part:
+        query = query.where(Workout.body_part == body_part)
+    if equipment:
+        query = query.where(Workout.equipment == equipment)
+    if level:
+        query = query.where(Workout.level == level)
+
+    workouts = db.exec(query.order_by(Workout.title)).all()
     routines = db.exec(select(Routine).where(Routine.user_id == user.id)).all()
+
+    types = [row[0] for row in db.exec(select(Workout.type).distinct().order_by(Workout.type)).all()]
+    body_parts = [row[0] for row in db.exec(select(Workout.body_part).distinct().order_by(Workout.body_part)).all()]
+    equipments = [row[0] for row in db.exec(select(Workout.equipment).distinct().order_by(Workout.equipment)).all()]
+    levels = [row[0] for row in db.exec(select(Workout.level).distinct().order_by(Workout.level)).all()]
+
     return templates.TemplateResponse(
         request=request,
         name="workouts.html",
@@ -18,5 +44,13 @@ async def workouts_view(request: Request, user: AuthDep, db: SessionDep):
             "user": user,
             "workouts": workouts,
             "routines": routines,
+            "types": types,
+            "body_parts": body_parts,
+            "equipments": equipments,
+            "levels": levels,
+            "selected_workout_type": workout_type,
+            "selected_body_part": body_part,
+            "selected_equipment": equipment,
+            "selected_level": level,
         },
     )
